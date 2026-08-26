d3754ed58ffd762134a76d401b6602b7273395e4
Felix Hanau <felix@cloudflare.com>
2026-08-26 09:10:43 -0400

    Simplify build configuration
    - Use ubuntu-24.04 runner so we get a more recent clang version
    - Use native arm64 build, getting rid of zig
    - Remove setup steps for dependencies that are already installed on the runner
      images
    - CLANG_ENABLE_ARCMT is deprecated, don't attempt to disable it
    - Fix thinLTO support

--- build/llvm/clang-lld-linux.cmake
+++ build/llvm/clang-lld-linux.cmake
@@ -1,6 +1,11 @@
 # Toolchain file to build LLVM using clang and link using lld
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_LINKER_TYPE LLD)
 
 # Tell LLVM's build system to link with lld instead of the default linker
 set(LLVM_USE_LINKER lld CACHE STRING "" FORCE)
+
+# Use LLVM versions of ar and ranlib to avoid warnings about thinLTO object files
+set(CMAKE_AR llvm-ar)
+set(CMAKE_RANLIB llvm-ranlib)
